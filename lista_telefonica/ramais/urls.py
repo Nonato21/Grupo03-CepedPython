66262842b17b4e7d0088af57c22fff068b29a190
Nonato21 <jvnonato39@gmail.com>
2026-07-04 17:15:39 -0300

    Implementa o CRUD completo de setores

--- lista_telefonica/ramais/urls.py
+++ lista_telefonica/ramais/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('setores/', views.listar_setores, name='listar_setores'),
     path('gerenciar/', views.gerenciar_ramais, name='gerenciar_ramais'),
     path('', views.index, name='index'),
+    path("setores/cadastrar/", views.cadastrar_setores,name="cadastrar_setores"),
+    path("setores/excluir/<int:id>/", views.deletar_setores,name="deletar_setores"),
+    path("setores/editar/<int:id>/", views.editar_setores, name="editar_setores")
     # espaço para adicionar mais rotas
 ]
--- lista_telefonica/ramais/views.py
+++ lista_telefonica/ramais/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
-
 # Create your views here.
-
+from django.contrib import messages
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Setor
 
 def listar_setores(request):
@@ -13,3 +12,211 @@ def gerenciar_ramais(request):
 
 def index(request):
     return render(request, 'ramais/index.html')
+
+def cadastrar_setores(request):
+
+    if request.method == "POST":
+
+        nome = request.POST["nome"].strip()
+        email = request.POST["email"].strip()
+        ramal = request.POST["ramal"].strip()
+    
+               
+        if nome == "":
+            return render(request,
+                          "ramais/gerenciar_ramais.html",
+                          {
+                              "nome":nome,
+                              "email":email,
+                              "ramal":ramal,
+                              "erro_nome": "Informe o nome do setor"
+                          })
+        
+        if email == "":
+            return render(request,
+                          "ramais/gerenciar_ramais.html",
+                          {
+                              "nome":nome,
+                              "email":email,
+                              "ramal":ramal,
+                              "erro_email": "Informe o nome do e-mail"
+                          })
+        
+        if ramal == "":
+            return render(request,
+                          "ramais/gerenciar_ramais.html",
+                          {
+                              "nome":nome,
+                              "email":email,
+                              "ramal":ramal,
+                              "erro_ramal": "Informe o número do ramal"
+                          })
+        
+        if Setor.objects.filter(nome__iexact=nome).exists():
+            messages.error(request, "Setor já cadastrado anteriormente, por favor adicione outro setor!!!")
+            return render(request,
+                          "ramais/gerenciar_ramais.html",
+                          {
+                            "nome": nome,
+                            "email":email,
+                            "ramal":ramal   
+                          })
+            
+            
+        if Setor.objects.filter(email__iexact=email).exists():
+            messages.error(request, "E-mail já cadastrado")
+            return render(request,
+                          "ramais/gerenciar_ramais.html",
+                          {
+                            "nome": nome,
+                            "email":email,
+                            "ramal":ramal   
+                          })
+        
+        if Setor.objects.filter(ramal=ramal).exists():
+            messages.error(request, "Ramal já cadastrado")
+            return render(request, "ramais/gerenciar_ramais.html", 
+                          {
+                            "nome": nome,
+                            "email":email,
+                            "ramal":ramal   
+                          })  
+        
+        if(len(ramal)!=4):      
+            messages.error(request, "O ramal deve ter exatamente 4 digitos!!!")
+            return render(request, "ramais/gerenciar_ramais.html", 
+                          {
+                            "nome": nome,
+                            "email":email,
+                            "ramal":ramal   
+                          })
+        
+        Setor.objects.create(
+            nome=nome,
+            email=email,
+            ramal=ramal
+        )
+        
+        messages.success(request, "Setor cadastrado com sucesso!!!")
+        
+
+        return redirect("listar_setores")
+
+    return render(request, "ramais/gerenciar_ramais.html")
+
+def deletar_setores(request, id):
+    
+    if request.method == "POST":
+        
+        setor = get_object_or_404(Setor, id=id)
+        
+        setor.delete()
+        
+        messages.success(request, "Setor excluído com sucesso!!!")
+        
+    return redirect("listar_setores")
+    
+def editar_setores(request, id):
+    
+    setor = get_object_or_404(Setor, id=id)
+    
+    if request.method == "POST":
+        nome = request.POST["nome"].strip()
+        email = request.POST["email"].strip()
+        ramal = request.POST["ramal"].strip()       
+                
+        if nome == "":
+            return render(request,
+                          "ramais/editar_ramais.html",
+                          { 
+                              "setor":setor,
+                              "nome":nome,
+                              "email":email,
+                              "ramal":ramal,
+                              "erro_nome": "Informe o nome do setor"
+                          })
+        
+        if email == "":
+            return render(request,
+                          "ramais/editar_ramais.html",
+                          {
+                              "setor":setor,
+                              "nome":nome,
+                              "email":email,
+                              "ramal":ramal,
+                              "erro_email": "Informe o nome do e-mail"
+                          })
+        
+        if ramal == "":
+            return render(request,
+                          "ramais/editar_ramais.html",
+                          {
+                              "setor":setor,
+                              "nome":nome,
+                              "email":email,
+                              "ramal":ramal,
+                              "erro_ramal": "Informe o número do ramal"
+                          })
+        
+        if Setor.objects.filter(nome__iexact=nome).exclude(id=id).exists():
+            messages.error(request, "Setor já cadastrado anteriormente, por favor adicione outro setor!")
+            return render(request,
+                          "ramais/editar_ramais.html",
+                          {
+                            "setor":setor,
+                            "nome": nome,
+                            "email":email,
+                            "ramal":ramal   
+                          })
+            
+            
+        if Setor.objects.filter(email__iexact=email).exclude(id=id).exists():
+            messages.error(request, "E-mail já cadastrado!!!")
+            return render(request,
+                          "ramais/editar_ramais.html",
+                          {
+                            "setor":setor,
+                            "nome": nome,
+                            "email":email,
+                            "ramal":ramal   
+                          })
+        
+        if Setor.objects.filter(ramal=ramal).exclude(id=id).exists():
+            messages.error(request, "Ramal já cadastrado!!!")
+            return render(request, "ramais/editar_ramais.html", 
+                          {
+                            "setor":setor,  
+                            "nome": nome,
+                            "email":email,
+                            "ramal":ramal   
+                          })  
+        
+        if(len(ramal)!=4):      
+            messages.error(request, "O ramal deve ter exatamente 4 digitos!!!")
+            return render(request, "ramais/editar_ramais.html", 
+                          {
+                            "setor":setor,
+                            "nome": nome,
+                            "email":email,
+                            "ramal":ramal   
+                          })
+        
+        setor.nome = nome
+        setor.email = email
+        setor.ramal = ramal
+        
+        setor.save()              
+        
+        messages.success(request, "Setor atualizado com sucesso!!!")
+        
+        return redirect("listar_setores")
+
+    return render(request, "ramais/editar_ramais.html",
+                  {
+                    "setor":setor
+                    })
+
+ 
+    
+
+
